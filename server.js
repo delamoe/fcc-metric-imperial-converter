328e4a0ee21543e334f2b5fcc8d7c3568af082d8
delamoe <delamoe@users.noreply.github.com>
2019-11-04 11:38:28 +0300

    small update to server.js to ensure it is still working

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 'use strict'
 // from only git-glitch
-const { execSync }    = require('child_process')
-const path            = require('path')
+const { execSync } = require('child_process')
+const path = require('path')
 
 // from git-glitch and FreeCodeCamp- Information Security and Quality Assurance
-const express         = require('express')
-const bodyParser      = require('body-parser')
-const app             = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
 // from only FreeCodeCamp- Information Security and Quality Assurance
-var expect            = require('chai').expect;
-var cors              = require('cors');
+var expect = require('chai').expect;
+var cors = require('cors');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+var apiRoutes = require('./routes/api.js');
+var fccTestingRoutes = require('./routes/fcctesting.js');
+var runner = require('./test-runner');
 
 app.use(bodyParser.json())
 
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 /* app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'README.md'))
@@ -33,30 +33,44 @@ app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
-  
-  const listener = app.listen(process.env.PORT, function() {
-    console.log('Your app is listening on port ' + listener.address().port);
-  })
-  
-  // git-glitch sync code ****************************
-  app.post('/deploy', (request, response) => {
-    if (request.query.secret !== process.env.SECRET) {
-      response.status(401).send()
-      return
-    }
-    
-    if (request.body.ref !== 'refs/heads/glitch') {
-      response.status(200).send('Push was not to glitch branch, so did not deploy.')
-      return
-    }
-    
-    const repoUrl = request.body.repository.git_url
-  
-    console.log('Fetching latest changes.')
-    const output = execSync(
-      `git checkout -- ./ && git pull -X theirs ${repoUrl} glitch && refresh`
-    ).toString()
-    console.log(output)
-    response.status(200).send()
-  })
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);
+
+//404 Not Found Middleware
+app.use(function (req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+})
+
+// git-glitch sync code ****************************
+app.post('/deploy', (request, response) => {
+  if (request.query.secret !== process.env.SECRET) {
+    response.status(401).send()
+    return
+  }
+
+  if (request.body.ref !== 'refs/heads/glitch') {
+    response.status(200).send('Push was not to glitch branch, so did not deploy.')
+    return
+  }
+
+  const repoUrl = request.body.repository.git_url
+
+  console.log('Fetching latest changes.')
+  const output = execSync(
+    `git checkout -- ./ && git pull -X theirs ${repoUrl} glitch && refresh`
+  ).toString()
+  console.log(output)
+  response.status(200).send()
+})
   // **************************************
