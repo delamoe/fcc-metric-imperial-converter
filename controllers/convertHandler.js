19476dca2bbe700e68fcf39242037d73b2414a38
delamoe <delamoe@users.noreply.github.com>
2019-11-04 13:26:58 +0300

    updating getNum & getUnit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -7,38 +7,45 @@
 */
 
 function ConvertHandler() {
-  
-  this.getNum = (input => parseInt(input.substring(0, input.search(/[\D\.\\]/))));
-  
-  this.getUnit = (input => parseInt(input.substring(input.search(/\D/), 0)));
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getNum = function (input) {
+    var result = parseInt(input.substring(0, input.search(/[\D\.\\]/)));
+    console.log(result);
+    return result;
+  }
+
+  this.getUnit = function (input) {
+    var result = arseInt(input.substring(input.search(/\D/), 0));
+    console.log(result);
+    return result;
+  }
+  this.getReturnUnit = function (initUnit) {
     var result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     var result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     var result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     var result;
-    
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
